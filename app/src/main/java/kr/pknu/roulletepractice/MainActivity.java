f93b294d278299c8531d0d374b74a434c496e633
Konrad <gimmy6707@gmail.com>
2024-11-24 16:09:02 +0900

    Roulette random color generator has been implemented:

---
+++ app/src/main/java/kr/pknu/roulletepractice/MainActivity.java
@@ -0,0 +1,120 @@
+package kr.pknu.roulletepractice;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.graphics.Bitmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bluehomestudio.luckywheel.LuckyWheel;
+import com.bluehomestudio.luckywheel.OnLuckyWheelReachTheTarget;
+import com.bluehomestudio.luckywheel.WheelItem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LuckyWheel luckyWheel;
+    private List<WheelItem> wheelItems = new ArrayList<>();
+    private String point;
+
+    public boolean Init = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        luckyWheel = findViewById(R.id.Roulette);
+
+        generateInitialWheelItems();
+
+        luckyWheel.setLuckyWheelReachTheTarget(new OnLuckyWheelReachTheTarget() {
+            @Override
+            public void onReachTarget() {
+                WheelItem wheelItem = wheelItems.get(Integer.parseInt(point) - 1);
+                String money = wheelItem.text;
+                Toast.makeText(MainActivity.this, money, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button addItem = findViewById(R.id.btnAdd);
+        EditText input = findViewById(R.id.edtItem);
+        addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!Init){
+                    wheelItems = new ArrayList<>();
+                    Init = true;
+                }
+                else{
+                    String item = input.getText().toString();
+                    addNewWheelItem(item);
+                }
+
+            }
+        });
+
+        Button start = findViewById(R.id.btnRoll);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!wheelItems.isEmpty()) {
+                    Random random = new Random();
+                    point = String.valueOf(random.nextInt(wheelItems.size()) + 1);
+                    luckyWheel.rotateWheelTo(Integer.parseInt(point));
+                } else {
+                    Toast.makeText(MainActivity.this, "Add items to the wheel first!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void generateInitialWheelItems() {
+        String[] defaultItems = {""};
+        for (String item : defaultItems) {
+            addNewWheelItem(item);
+        }
+    }
+
+    private void addNewWheelItem(String input) {
+        if(input.isEmpty()){
+            Toast.makeText(MainActivity.this,"Please Add item", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Drawable d = getResources().getDrawable(R.drawable.ic_money, null);
+            Bitmap bitmap = drawableToBitmap(d);
+            WheelItem newItem = new WheelItem(generateRandomColor(), bitmap, input);
+            wheelItems.add(newItem);
+
+            // 전체 리스트를 다시 설정
+            List<WheelItem> updatedWheelItems = new ArrayList<>(wheelItems);
+            luckyWheel.addWheelItems(updatedWheelItems);
+        }
+    }
+
+    private int generateRandomColor(){
+        Random random = new Random();
+        return Color.rgb(random.nextInt(256),random.nextInt(256),random.nextInt(256));
+    }
+
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+}
